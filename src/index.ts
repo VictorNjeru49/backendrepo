8861eacb37f591bba4cac127d03df6fb49b735d6
victornjeru49 <victornjeru49@gmail.com>
2024-06-27 15:22:06 +0300

    fourth commit

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
 import { authorRouter } from './author/author.router'
 import { BookRouter } from './books/book.router'
+import { cors} from 'hono/cors'
 
 const app = new Hono()
 
@@ -12,6 +13,10 @@ app.get('/', (c) => {
 app.route('/',BookRouter)
 app.route('/',authorRouter)
 
+app.use('*', cors({
+  origin: 'http://localhost:5173/',
+  allowMethods: ['GET', 'POST', 'PUT', 'DELETE'],
+}));
 serve({
   fetch: app.fetch,
   port: Number(process.env.PORT )
